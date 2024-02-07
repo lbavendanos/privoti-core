cfc66019bca06a1ba7b4e65ed6273c489f2d5809
lbavendanos <lbavendanos@gmail.com>
2024-02-07 18:15:27 -0500

    Add address and contact models

--- database/migrations/2024_02_07_224726_create_addresses_table.php
+++ database/migrations/2024_02_07_224726_create_addresses_table.php
@@ -11,16 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('last_name');
-            $table->date('dob')->nullable();
-            $table->string('phone')->nullable();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('phone');
+            $table->string('address1');
+            $table->string('address2')->nullable();
+            $table->string('district');
+            $table->string('city');
+            $table->string('state');
+            $table->boolean('default')->default(false);
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('addresses');
     }
 };
