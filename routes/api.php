743e3d873ce60fc59ce58fe2c591fb7b2d1b6c12
lbavendanos <lbavendanos@gmail.com>
2024-08-18 16:02:26 -0500

    Fix api routes

--- routes/api.php
+++ routes/api.php
@@ -8,14 +8,16 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/auth/user', [UserController::class, 'index']);
 
     Route::middleware(['verified'])->group(function () {
-        Route::put('/auth/user', [UserController::class, 'update']);
-        Route::post('/auth/user/password', [UserController::class, 'updatePassword']);
-        Route::post('/auth/user/email/notification', [UserController::class, 'sendEmailChangeVerificationNotification']);
-        Route::get('/auth/user/email/verify/{id}/{email}/{hash}', [UserController::class, 'verifyNewEmail'])
-            ->middleware(['signed', 'throttle:6,1'])
-            ->name('auth.user.email.verify');
+        Route::prefix('auth')->group(function () {
+            Route::put('/user', [UserController::class, 'update']);
+            Route::post('/user/password', [UserController::class, 'updatePassword']);
+            Route::post('/user/email/notification', [UserController::class, 'sendEmailChangeVerificationNotification']);
+            Route::get('/user/email/verify/{id}/{email}/{hash}', [UserController::class, 'verifyNewEmail'])
+                ->middleware(['signed', 'throttle:6,1'])
+                ->name('auth.user.email.verify');
 
-        Route::apiResource('/auth/addresses', AddressController::class);
-        Route::put('/auth/addresses/{address}/default', [AddressController::class, 'setDefault']);
+            Route::apiResource('/addresses', AddressController::class);
+            Route::put('/addresses/{address}/default', [AddressController::class, 'setDefault']);
+        });
     });
 });
