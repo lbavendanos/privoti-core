a8f9fc2af2130e98c54f1ab32901078549247e50
lbavendanos <lbavendanos@gmail.com>
2024-08-26 19:29:09 -0500

    Add admin functionality

---
+++ app/Domains/D/Notifications/VerifyNewEmail.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Domains\D\Notifications;
+
+use App\Models\Admin;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\URL;
+
+class VerifyNewEmail extends Notification
+{
+    use Queueable;
+
+    public $admin;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Admin $admin)
+    {
+        $this->admin = $admin;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $verificationUrl = $this->verificationUrl($notifiable);
+
+        return $this->buildMailMessage($verificationUrl);
+    }
+
+    /**
+     * Get the verify new email notification mail message for the given URL.
+     *
+     * @param  string  $url
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    protected function buildMailMessage($url)
+    {
+        return (new MailMessage)
+            ->subject(Lang::get('Verify New Email Address'))
+            ->line(Lang::get('Please click the button below to verify your new email address.'))
+            ->action(Lang::get('Verify New Email Address'), $url)
+            ->line(Lang::get('If you did not request an change email, no further action is required.'));
+    }
+
+    /**
+     * Get the verification URL for the given notifiable.
+     *
+     * @param  mixed  $notifiable
+     * @return string
+     */
+    protected function verificationUrl($notifiable)
+    {
+        $type = 'verify-new-email';
+        $id = $this->admin->getKey();
+        $email = $notifiable->routes['mail'];
+        $hash = sha1($notifiable->routes['mail']);
+
+        $temporarySignedRoute = URL::temporarySignedRoute(
+            'auth.admin.email.new.verify',
+            now()->addMinutes(config('auth.verification.expire', 60)),
+            ['id' => $id, 'email' => $email, 'hash' => $hash]
+        );
+
+        $query = parse_url($temporarySignedRoute, PHP_URL_QUERY);
+
+        return config('app.frontend_url') . "/auth/confirm?type={$type}&id={$id}&email={$email}&token={$hash}&{$query}";
+    }
+}
--- app/Domains/D/Routes/auth.php
+++ app/Domains/D/Routes/auth.php
@@ -16,5 +16,15 @@ Route::prefix('auth')->group(function () {
     Route::middleware(['auth:dashboard'])->group(function () {
         Route::post('/logout', [AuthController::class, 'destroy']);
         Route::get('/admin', [AdminController::class, 'index']);
+
+        Route::middleware(['verified'])->group(function () {
+            Route::put('/admin', [AdminController::class, 'update']);
+            Route::post('/admin/password', [AdminController::class, 'updatePassword']);
+            Route::post('/admin/email/new/notification', [AdminController::class, 'sendEmailChangeVerificationNotification'])
+                ->middleware(['throttle:6,1']);
+            Route::get('/admin/email/new/verify/{id}/{email}/{hash}', [AdminController::class, 'verifyNewEmail'])
+                ->middleware(['signed', 'throttle:6,1'])
+                ->name('auth.admin.email.new.verify');
+        });
     });
 });
