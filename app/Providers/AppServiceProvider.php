5b3b04faa4c5a80a7f2e70b0fe62d7e042865de5
lbavendanos <lbavendanos@gmail.com>
2024-08-31 13:20:34 -0500

    Refactor app service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,6 +11,11 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function isUser(object $notifiable): bool
+    {
+        return get_class($notifiable) === \App\Models\User::class;
+    }
+
     /**
      * Register any application services.
      */
@@ -25,14 +30,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            $baseUrl = get_class($notifiable) === \App\Models\User::class ? config('app.store_url') : config('app.dashboard_url');
+            $baseUrl = $this->isUser($notifiable) ? config('app.store_url') : config('app.dashboard_url');
 
             return $baseUrl . "/password/reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
 
         VerifyEmail::createUrlUsing(function (object $notifiable) {
-            $baseUrl = get_class($notifiable) === \App\Models\User::class ? config('app.store_url') : config('app.dashboard_url');
-            $modelName = get_class($notifiable) === \App\Models\User::class ? 'user' : 'admin';
+            $baseUrl = $this->isUser($notifiable) ? config('app.store_url') : config('app.dashboard_url');
+            $modelName = $this->isUser($notifiable) ? 'user' : 'admin';
 
             $type = 'verify-email';
             $id = $notifiable->getKey();
