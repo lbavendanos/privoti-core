119db7aa4b9f3756bf01f6aa3cd620e415c21798
lbavendanos <lbavendanos@gmail.com>
2024-11-13 20:34:31 -0500

    Add accessors to admin model

--- app/Domains/Cms/Http/Resources/AdminResource.php
+++ app/Domains/Cms/Http/Resources/AdminResource.php
@@ -14,6 +14,20 @@ class AdminResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'full_name' => $this->full_name,
+            'short_name' => $this->short_name,
+            'initials' => $this->initials,
+            'email' => $this->email,
+            'avatar' => null,
+            'phone' => $this->phone,
+            'dob' => $this->dob,
+            'email_verified_at' => $this->email_verified_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -46,5 +47,26 @@ class Admin extends Authenticatable implements MustVerifyEmail
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected function fullName(): Attribute
+    {
+        return Attribute::make(
+            get: fn(mixed $value, array $attributes) => $attributes['first_name'] . ' ' . $attributes['last_name']
+        );
+    }
+
+    protected function shortName(): Attribute
+    {
+        return Attribute::make(
+            get: fn(mixed $value, array $attributes) => trim(ucwords(explode(' ', $attributes['first_name'])[0] . ' ' . explode(' ', $attributes['last_name'])[0]))
+        );
+    }
+
+    protected function initials(): Attribute
+    {
+        return Attribute::make(
+            get: fn(mixed $value, array $attributes) => strtoupper($attributes['first_name'][0] . strtoupper($attributes['last_name'][0]))
+        );
     }
 }
