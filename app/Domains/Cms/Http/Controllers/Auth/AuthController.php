e0a9c6decfcfea24bf55adb05512ee210a3a96d1
lbavendanos <lbavendanos@gmail.com>
2024-12-01 21:15:21 -0500

    Fix forgot password response

--- app/Domains/Cms/Http/Controllers/Auth/AuthController.php
+++ app/Domains/Cms/Http/Controllers/Auth/AuthController.php
@@ -94,7 +94,7 @@ class AuthController extends Controller
             ]);
         }
 
-        return response()->json(['status' => __($status)]);
+        return response()->json(['message' => __($status)]);
     }
 
     /**
