20ef2f67b6c3b420c2f83819a851d308be09c6cf
lbavendanos <lbavendanos@gmail.com>
2025-02-14 02:03:50 -0500

    Add product media model

---
+++ database/migrations/2025_02_14_065141_create_product_media_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_media', function (Blueprint $table) {
+            $table->id();
+            $table->string('path');
+            $table->enum('type', ['image', 'video']);
+            $table->integer('position')->default(0);
+            $table->foreignId('product_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_media');
+    }
+};
