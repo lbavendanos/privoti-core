a84dad9218c4966b9f43f07f5712ea3ee2138c0d
lbavendanos <lbavendanos@gmail.com>
2025-02-18 16:08:02 -0500

    Add categories and types factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\ProductCategory;
+use App\Models\ProductType;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -19,13 +20,49 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('abc123..'),
         ]);
 
-        ProductCategory::factory()
-            ->count(3)
+        $categories = [
+            'Men' => ['Shirts', 'T-Shirts', 'Pants', 'Jackets', 'Shoes'],
+            'Women' => ['Dresses', 'Tops', 'Skirts', 'Pants', 'Jackets', 'Shoes'],
+            'Kids' => [
+                'Boys' => ['Shirts', 'Pants', 'Shoes'],
+                'Girls' => ['Dresses', 'Skirts', 'Shoes'],
+            ],
+            'Accessories' => ['Bags', 'Hats', 'Scarves', 'Sunglasses']
+        ];
+
+        $this->createCategory($categories);
+
+        ProductType::factory()
+            ->count(7)
             ->sequence(
-                ['name' => 'Shirts', 'handle' => 'shirts'],
-                ['name' => 'Sweatshirts', 'handle' => 'sweatshirts'],
-                ['name' => 'Pants', 'handle' => 'pants'],
+                ['name' => 'Casual'],
+                ['name' => 'Formal'],
+                ['name' => 'Sport'],
+                ['name' => 'Business'],
+                ['name' => 'Party'],
+                ['name' => 'Outerwear'],
+                ['name' => 'Footwear'],
             )
             ->create();
+    }
+
+    /**
+     * Create categories recursively.
+     *
+     * @param array $categories
+     * @param int|null $parent_id
+     */
+    function createCategory($categories, $parent_id = null)
+    {
+        foreach ($categories as $category => $subcategories) {
+            if (is_array($subcategories)) {
+                $parent = ProductCategory::factory()->create(['name' => $category, 'parent_id' => $parent_id]);
+
+                $this->createCategory($subcategories, $parent->id);
+                continue;
+            }
+
+            $parent = ProductCategory::factory()->create(['name' => $subcategories, 'parent_id' => $parent_id]);
+        }
     }
 }
