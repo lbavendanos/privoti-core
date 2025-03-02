781a9d4b5177e084620e3981f6f52f56f7037a91
lbavendanos <lbavendanos@gmail.com>
2025-03-02 12:57:51 -0500

    Add vendor api

--- app/Domains/Cms/Routes/api.php
+++ app/Domains/Cms/Routes/api.php
@@ -3,6 +3,7 @@
 use App\Domains\Cms\Http\Controllers\ProductCategoryController;
 use App\Domains\Cms\Http\Controllers\ProductController;
 use App\Domains\Cms\Http\Controllers\ProductTypeController;
+use App\Domains\Cms\Http\Controllers\VendorController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('c')->group(function () {
@@ -12,5 +13,6 @@ Route::prefix('c')->group(function () {
         Route::apiResource('products', ProductController::class);
         Route::apiResource('categories', ProductCategoryController::class);
         Route::apiResource('types', ProductTypeController::class);
+        Route::apiResource('vendors', VendorController::class);
     });
 });
