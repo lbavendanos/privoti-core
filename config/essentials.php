4133d4c262db9284b3aae3c4249ef0ba52770f10
lbavendanos <lbavendanos@gmail.com>
2025-09-05 02:21:32 -0500

    Fix essentials config

--- config/essentials.php
+++ config/essentials.php
@@ -3,7 +3,5 @@
 declare(strict_types=1);
 
 return [
-    // NunoMaduro\Essentials\Configurables\ShouldBeStrict::class => true,
     NunoMaduro\Essentials\Configurables\ForceScheme::class => false,
-    // other configurables...
 ];
